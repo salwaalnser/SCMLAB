d8664f27333bcd52dda2eddacdb843d8a66bb0cb
2221133318 <2221133318@CPES-3B1032-P12.ku.edu.kw>
2025-11-10 15:19:27 +0300

    Second Commit

--- src/SCMLAB.java
+++ src/SCMLAB.java
@@ -2,9 +2,8 @@
 public class SCMLAB {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 		System.out.println("Hello World");
+		System.out.println("Second");
 	}
-
+	
 }
